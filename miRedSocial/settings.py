36d015e656369d7ff226d8d285ad3b5135661832
Alexander Francisco Segovia Razo <asegoviar.uchile@gmail.com>
2025-12-12 16:55:12 -0500

    VersionFinal

--- miRedSocial/settings.py
+++ miRedSocial/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dhbut!@)zvcwyf$)*&%ei#)o$b(kt*tf=#)^!#^x-+06i%m&@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
