38becae32c0a185fbff3ab201fbbe2b1c3435532
Alexander Francisco Segovia Razo <asegoviar.uchile@gmail.com>
2025-12-12 17:24:41 -0500

    Prueba de autotest

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+
+class ProjectStartupTest(TestCase):
+    def test_project_starts(self):
+        """
+        El proyecto Django arranca correctamente.
+        """
+        self.assertIsNotNone(settings.SECRET_KEY)
+
+
+class LoginPageTest(TestCase):
+    def test_login_page_returns_200(self):
+        """
+        Test 2:
+        La ruta /login responde con HTTP 200.
+        """
+        response = self.client.get("/login")
+        self.assertEqual(response.status_code, 200)
+
+class LoginTemplateTest(TestCase):
+    def test_login_uses_correct_template(self):
+        """
+        Test 3:
+        Verifica que la vista de login usa el template correcto.
+        """
+        response = self.client.get("/login/")
+        self.assertTemplateUsed(response, "accounts/login.html")
